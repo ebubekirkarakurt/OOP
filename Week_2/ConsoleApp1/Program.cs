b69641834bea2ab95bc6f9d53e595f69a6df1c7b
Ebubekir KARAKURT <114517935+ebubekirkarakurt@users.noreply.github.com>
2023-10-22 23:59:59 +0300

    first commit

---
+++ Week_2/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿Account account = new Account();
+account.FirstName = "Ebubekir";
+account.LastName = "Karakurt";
+
+account.Deposit(700);
+account.Withdraw(1900);
+account.Withdraw(200);
+account.AccountActivities();
+account.CheckAccount();
