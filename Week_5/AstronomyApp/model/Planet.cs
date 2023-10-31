b516881837aa641421f68f713a5703ccb4dfd85f
Ebubekir KARAKURT <114517935+ebubekirkarakurt@users.noreply.github.com>
2023-10-31 16:22:26 +0300

    Week_5 added

---
+++ Week_5/AstronomyApp/model/Planet.cs
@@ -0,0 +1,17 @@
+class Planet : CelestialBody
+{
+
+    public double Distance { get; set; }
+    public double Gravity { get; set; }
+
+    public Planet(string name, double mass, double radius, double distance,double gravity) : base(name, mass, radius)
+    {
+        this.Gravity = gravity;
+        this.Distance = distance;
+    }
+
+    public override void Describe()
+    {
+        System.Console.WriteLine($"{Name}\n{Mass}\n{Radius}\n{Distance}\n{Gravity}");
+    }
+}
